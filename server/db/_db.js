ce9183b46f3fc740c78aa175b1d2ba1b855c0890
Rosana Liang <rosanajliang@gmail.com>
2016-09-25 20:38:52 -0400

    native true

--- server/db/_db.js
+++ server/db/_db.js
@@ -3,13 +3,10 @@ var db = null;
 
 if (process.env.DATABASE_URL) {
    // the application is executed on Heroku ... use the postgres database
-   db = new Sequelize(process.env.DATABASE_URL, {
-     dialect:  'postgres',
-     protocol: 'postgres',
-     port:     match[4],
-     host:     match[3],
-     logging:  true //false
-   });
+    db = new Sequelize(process.env.DATABASE_URL, {
+        native: true,
+        logging:  true //false
+    });
 
 } else {
     db = new Sequelize('postgres://localhost:5432/goclimb', { logging: false });
